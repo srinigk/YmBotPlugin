e441cd95f847a0a46ec00f0c9d7c174d9c020686
G Srinivasa <srinivasa.g@connectm.com>
2021-01-01 18:39:56 +0530

    Initial Commit

---
+++ YmBotPlugin/YmBotPlugin.h
@@ -0,0 +1,19 @@
+//
+//  YmBotPlugin.h
+//  YmBotPlugin
+//
+//  Created by G Srinivasa on 01/01/21.
+//  Copyright © 2021 G Srinivasa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for YmBotPlugin.
+FOUNDATION_EXPORT double YmBotPluginVersionNumber;
+
+//! Project version string for YmBotPlugin.
+FOUNDATION_EXPORT const unsigned char YmBotPluginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <YmBotPlugin/PublicHeader.h>
+
+
